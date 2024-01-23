c756b48f9dfa939c7c9ddd9db1829aa7ed39ea1a
pawel <pawel.marcisz1@gmail.com>
2024-01-23 19:45:53 +0100

    Add clear button handler

--- WPFPaint/MainWindow.xaml.cs
+++ WPFPaint/MainWindow.xaml.cs
@@ -16,9 +16,17 @@ namespace WPFPaint
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private bool eraseMode = false;
+        private bool selectMode = false;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            inkCanvas.Strokes.Clear();
         }
     }
 }
